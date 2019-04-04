d3e54b2e170fe423eb32c2872bbaac88decee59d
Darrion Gering and Mathew Akre <mathew@>
2019-04-04 15:54:02 -0700

    Add determineLength and createSquare functions

---
+++ README.md
@@ -0,0 +1,36 @@
+# Roman numerals
+
+#### Number to roman numeral converter, 3/22/2019
+
+#### By **Mathew A and Darrion G**
+
+## Description
+
+_ Takes in a sentence and returns a crypto message
+
+
+## Behavior-Driven Development
+
+|Behavior|Input|Output|
+|-|-|-|
+|message length determines col/rows| "Have a nice day| letters < 16, 4x4|
+|Takes a coded message and puts it into rows and columns|word word|[word][word]|
+|Message length doesnt fit| "dontcompare" | dont comp are|
+|Slices array into 5 character increments|Have a nice day|hifei acedl|
+
+## Setup/Installation Requirements
+
+1. Clone Github repository
+2. Open index.html
+
+## Technologies Used
+
+1. html
+2. css
+3. javascript
+
+### License
+
+*MIT license*
+
+Copyright (c) 2016 **_Mathew A and Darrion G**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+//Business logic
+function determineLength(sentence) {
+  var length = sentence.length;
+  if(length <= 4) {
+    var size = 2;
+  } else if(length <= 9) {
+    var size = 3;
+  } else if(length <= 16) {
+    var size = 4;
+  } else if(length <= 25) {
+    var size = 5;
+  } else if(length <= 36) {
+    var size = 6;
+  } else if(length <= 49) {
+    var size = 7;
+  } else if(length <= 64) {
+    var size = 8;
+  } else if(length <= 81) {
+    var size = 9;
+  }
+  return size;
+}
+
+function createSquare(size, sentence) {
+  i = 0;
+  var squareArray = []
+  for(var col=0; col < size; col++) {
+    for(var row=0; row < size; row++) {
+      i++;
+      squareArray.push(sentence[i])
+    }
+  }
+  console.log(squareArray);
+  console.log(size)
+}
 
 
 //user interface
@@ -5,7 +40,8 @@ $(document).ready(function() {
   $("#user").submit(function(event) {
     event.preventDefault();
     var sentence = $("input#sentence").val();
-    var result = conversion(sentence);
+    var squareSize = determineLength(sentence);
+    createSquare(squareSize, sentence);
     $("#result").show();
     $("#result").text(result);
   });
